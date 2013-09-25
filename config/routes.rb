1826b6844b26e0fd3855d583f47c7d61b5fefdb7
mixser <mixser@mixser-Lenovo-U410.(none)>
2013-09-25 12:44:20 +0300

    ruby 2.0.0

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,12 @@ Vksmash::Application.routes.draw do
   get "session_auth" => 'users#session_auth'
 
   resources :users
+
+  root 'pages#home'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'pages#about'
+  
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
